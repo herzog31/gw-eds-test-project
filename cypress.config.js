c2d8a96c30183b42e58e87f43d569e8a6859b67a
Mark J. Becker <herzog31@users.noreply.github.com>
2023-12-06 14:16:46 +0100

    Update cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,12 @@
 const { defineConfig } = require("cypress");
 
-// Calculate base url based on environment
+// Define baseUrl based on environment
 const { CI, GITHUB_REF_NAME, GITHUB_REPOSITORY } = process.env;
 let baseUrl = "http://localhost:3000";
 if (CI && GITHUB_REF_NAME && GITHUB_REPOSITORY) {
-    const [owner, repo] = GITHUB_REPOSITORY.split('/')
+    const [owner, repo] = GITHUB_REPOSITORY.split('/');
     baseUrl = `https://${GITHUB_REF_NAME}--${repo}--${owner}.hlx.live`;
 }
-
 console.log(`Using base url: ${baseUrl}`);
 
 module.exports = defineConfig({
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,9 +1,9 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
+describe('Sample tests', () => {
+  it('compares true with true', () => {
     expect(true).to.equal(true)
   })
 
-  it('checks the homepage', () => {
+  it('checks the title of the homepage', () => {
     cy.visit('/');
     cy.contains('h1', 'Congrats, you are ready to go!');
   });
