bed8e9c432c4e232976a8195e37f78630a849d8d
Mark J. Becker <herzog31@users.noreply.github.com>
2023-12-06 14:02:46 +0100

    Add URL mapping for tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
+// Calculate base url based on environment
+const { CI, GITHUB_REF_NAME } = process.env;
+let baseUrl = "http://localhost:3000";
+if (CI && GITHUB_REF_NAME) {
+    baseUrl = `https://${GITHUB_REF_NAME}--gw-eds-test-project--herzog31.hlx.live`;
+}
+
+console.log(`Using base url: ${baseUrl}`);
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl,
   },
 });
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -2,4 +2,9 @@ describe('My First Test', () => {
   it('Does not do much!', () => {
     expect(true).to.equal(true)
   })
+
+  it('checks the homepage', () => {
+    cy.visit('/');
+    cy.contains('h1', 'Congrats, you are ready to go!');
+  });
 });
